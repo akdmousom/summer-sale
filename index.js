a3c4db261b24bd6a732be5028988e9f709c6b26e
akdmausom <contact.mousom@gmail.com>
2023-09-13 05:08:46 +0600

    Summer Sale App Version 1.0

---
+++ index.js
@@ -0,0 +1,77 @@
+let count = 1;
+
+
+
+function productItem(event) {
+
+    const productName = event.childNodes[3].childNodes[1].innerText;
+    const productPrice = parseInt(event.childNodes[3].childNodes[5].innerText.split(" ")[0]);
+    let totalPrice = parseFloat(document.querySelector('.total').innerText.split(" ")[0]);
+    let intotal = productPrice + totalPrice;
+
+    document.querySelector('.total').innerText = intotal + " TK";
+    document.querySelector('.total-price-with-discount').innerText = intotal + " TK";
+
+    const parentElement = document.querySelector('.list');
+
+    count + 1
+
+    const createElementP = document.createElement("p");
+
+    createElementP.classList.add('text-2xl')
+    createElementP.classList.add('font-medium')
+
+    createElementP.innerText = count++ + ". " + productName;
+    parentElement.appendChild(createElementP);
+   
+    if (intotal > 0) {
+
+        document.querySelector('.make-pay').removeAttribute('disabled')
+
+
+    }
+
+    if (intotal >= 200) {
+        document.querySelector('.coupon-apply').removeAttribute('disabled')
+    }
+}
+
+
+document.getElementById('coupon-input').addEventListener('keyup', function (event) {
+
+    const text = event.target.value;
+
+    document.querySelector('.coupon-apply').addEventListener('click', function () {
+
+        if (text === 'SELL200') {
+
+            const toTalPrice = parseFloat(document.querySelector('.total-price').innerText.split(" ")[2]);
+
+            const disRatio = 0.2;
+
+            const disCountPrice = (toTalPrice * disRatio) - toTalPrice;
+
+            const ammountDiscount = toTalPrice - Math.abs(disCountPrice);
+
+            document.querySelector('.discount-taka').innerHTML = `${ammountDiscount.toFixed(2)} TK`;
+
+
+            document.querySelector('.total-price-with-discount').innerHTML = `${Math.abs(disCountPrice).toFixed(2)} TK`;
+
+
+        } else {
+            return
+        }
+    })
+})
+
+document.querySelector('.modal-container').childNodes[7].addEventListener('click', function (event) {
+
+    window.location.href = '/'
+    
+})
+
+
+
+
+
